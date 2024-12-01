5cc88ffea2b400f040aadd1973bcb6daa8738dc4
rytst <r.saito0111@gmail.com>
2024-12-01 20:34:28 +0900

    introduce add and sub

--- chibicc.c
+++ chibicc.c
@@ -3,15 +3,34 @@
 
 int main(int argc, char **argv) {
     if (argc != 2) {
-        fprintf(stderr, "Invalid number of arguments");
+        fprintf(stderr, "Invlid number of arguments.\n");
         return 1;
     }
 
-    printf(".intel_syntax noprefix\n");
-    printf(".global main\n");
-    printf("main:\n");
-    printf("  mov rax, %d\n", atoi(argv[1]));
-    printf("  ret\n");
+    char *p = argv[1];
 
+    printf(".intel_syntax noprefix\n");
+    printf(".globl main\n");
+    printf("main:\n");
+    printf("    mov rax, %ld\n", strtol(p, /* output param */ &p, 10));
+
+    while (*p) {
+        if (*p == '+') {
+            p++;
+            printf("    add rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        if (*p == '-') {
+            p++;
+            printf("    sub rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        fprintf(stderr, "Unexpected character: '%c'\n", *p);
+        return 1;
+    }
+
+    printf("    ret\n");
     return 0;
 }
